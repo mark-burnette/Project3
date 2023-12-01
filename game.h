86c8ee6f1f6f19ab306a47dd70f0633e44e52389
Chloe Nolan <116681534+chloekaii@users.noreply.github.com>
2023-12-01 11:47:47 -0500

    Update game.h

--- game.h
+++ game.h
@@ -8,13 +8,14 @@ class Game {
     vector<string> genres;
     string website;
     vector<string> platforms;
+    vector<string> developers;
     string esrb;
     int metacritic_rating;
     float user_rating;
     int num_user_ratings;
-    int num_user_suggestions;
-    public:
-        void printGameInfo();
-        void setRecScore(); //how we will rank the games, I was thinking with a combination of metacritic & user ratings
-        float recommendation_score; //a score of how likely we are to recommend this game based on what the user entered
+public:
+    Game();
+    Game(string name, string genres, string website, string platforms, string esrb, string developers, int metacritic_rating, float user_rating, int num_user_ratings, int num_user_suggestions);
+    void printGameInfo();
+    float setRecScore(); //how we will rank the games, I was thinking with a combination of metacritic & user ratings
 };
